3d0e30e06e935c0e45e0256ce262deb2355c2661
BrayenAngel <54649467+WhoImClay@users.noreply.github.com>
2019-12-12 07:51:01 +0700

    Add files via upload

---
+++ ʀᴀɪʜᴀɴ ғɪʀᴅ - ʙᴀsᴇ sᴄʀɪᴘᴛ ғғ ᴠ1.lua
@@ -0,0 +1,511 @@
+gg.alert('ɪsɪ ɴᴀᴍᴀ ғᴏʀᴍᴀᴛ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ ᴋᴀᴍᴜ !!')
+gg.toast('-ɴᴀᴍᴀ ᴋᴀᴍᴜ-')
+
+
+function Main()
+menu = gg.choice({
+'ᴍᴇɴᴜ_1',
+'ᴍᴇɴᴜ_2',
+'ᴍᴇɴᴜ_3',
+'ᴏᴜᴛ'},
+nil,'-ɪsɪ ɴᴀᴍᴀ ᴄʜᴀɴɴᴇʟ ᴋᴀᴍᴜ-\n -ʙᴀsᴇ sᴄʀɪᴘᴛ ᴠᴇʀsɪ ᴍᴇɴᴜ-')
+if menu == 1 then A() end
+if menu == 2 then B() end
+if menu == 3 then C() end
+if menu == 4 then Exit() end
+XGCK=-1
+end
+
+
+
+function A()
+menu1 = gg.multiChoice({
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_1',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_2',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_3',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_4',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_5',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_6',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_7',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_8',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_9',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_10',
+'ʙᴀᴄᴋ ᴛᴏ ᴍᴇɴᴜ'},
+nil,'ɪsɪ ɴᴀᴍᴀ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ')
+if menu1 == nil then else
+if menu1[1] == true then a1() end
+if menu1[2] == true then a2() end
+if menu1[3] == true then a3() end
+if menu1[4] == true then a4() end
+if menu1[5] == true then a5() end
+if menu1[6] == true then a6() end
+if menu1[7] == true then a7() end
+if menu1[8] == true then a8() end
+if menu1[9] == true then a9() end
+if menu1[10] == true then a10() end
+if menu1[11] == true then HOME() end
+end
+GLWW=-1
+end
+
+
+
+function B()
+menu1 = gg.multiChoice({
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_1',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_2',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_3',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_4',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_5',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_6',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_7',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_8',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_9',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_10',
+'ʙᴀᴄᴋ ᴛᴏ ᴍᴇɴᴜ'},
+nil,'ɪsɪ ɴᴀᴍᴀ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ')
+if menu1 == nil then else
+if menu1[1] == true then b1() end
+if menu1[2] == true then b2() end
+if menu1[3] == true then b3() end
+if menu1[4] == true then b4() end
+if menu1[5] == true then b5() end
+if menu1[6] == true then b6() end
+if menu1[7] == true then b7() end
+if menu1[8] == true then b8() end
+if menu1[9] == true then b9() end
+if menu1[10] == true then b10() end
+if menu1[11] == true then HOME() end
+end
+GLWW=-1
+end
+
+
+
+function C()
+menu1 = gg.multiChoice({
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_1',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_2',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_3',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_4',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_5',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_6',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_7',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_8',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_9',
+'ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_10',
+'ʙᴀᴄᴋ ᴛᴏ ᴍᴇɴᴜ'},
+nil,'ɪsɪ ɴᴀᴍᴀ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ')
+if menu1 == nil then else
+if menu1[1] == true then c1() end
+if menu1[2] == true then c2() end
+if menu1[3] == true then c3() end
+if menu1[4] == true then c4() end
+if menu1[5] == true then c5() end
+if menu1[6] == true then c6() end
+if menu1[7] == true then c7() end
+if menu1[8] == true then c8() end
+if menu1[9] == true then c9() end
+if menu1[10] == true then c10() end
+if menu1[11] == true then HOME() end
+end
+GLWW=-1
+end
+
+
+
+function a1()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a2()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a3()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a4()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a5()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a6()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a7()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a8()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a9()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function a10()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b1()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b2()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b3()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b4()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b5()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b6()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b7()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b8()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b9()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b10()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c1()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c2()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c3()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c4()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c5()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c6()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c7()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c8()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c9()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c10()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function Exit()
+print("ɪsɪ ғᴏʀᴍᴀᴛ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ ᴋᴀᴍᴜ")
+os.exit()
+end
+
+
+
+function HOME()
+lw=1
+Main()
+end
+
+
+
+cs = '这里可以填QQ'
+while(true)do
+if gg.isVisible(true) then
+XGCK=1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK==1 then
+Main()
+end
+end
+
+
+
+
+
+
+
+
+
+
