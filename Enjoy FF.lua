2dd152cf9039734f5841d043378f361050edff22
BrayenAngel <54649467+WhoImClay@users.noreply.github.com>
2019-12-12 11:47:41 +0700

    Add files via upload

---
+++ Enjoy FF.lua
@@ -0,0 +1,203 @@
+gg.alert('ɪsɪ ɴᴀᴍᴀ ғᴏʀᴍᴀᴛ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ ᴋᴀᴍᴜ !!')
+gg.toast('-ɴᴀᴍᴀ ᴋᴀᴍᴜ-')
+
+
+function Main()
+  SN = gg.choice({
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_1",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_2",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ 3",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_4",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_5",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_6",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_7",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_8",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_9",
+  "ɪsɪ ɴᴀᴍᴀ ғᴇᴀᴛᴜʀᴇ_10",
+  "ᴏᴜᴛ"
+ }, nil, "-ɪsɪ ɴᴀᴍᴀ ᴄʜᴀɴɴᴇʟ ᴋᴀᴍᴜ-\n -ʙᴀsᴇ sᴄʀɪᴘᴛ ᴠᴇʀsɪ ᴄʟɪᴄᴋ ʟᴏᴀᴅ-")
+  if SN == 1 then
+    a()
+  end
+  if SN == 2 then
+   b()
+  end
+  if SN == 3 then
+   c()
+  end
+  if SN == 4 then
+   d()
+  end
+  if SN == 5 then
+   e()
+  end
+  if SN == 6 then
+   f()
+  end
+  if SN == 7 then
+   g()
+  end
+  if SN == 8 then
+   h()
+  end
+  if SN == 9 then
+   i()
+  end
+  if SN == 10 then
+   j()
+  end
+  if SN == 11 then
+   Exit()
+  end
+  XGCK = -1
+end
+
+
+
+function a()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值",  gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function b()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function c()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function d()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function e()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function f()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function g()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function h()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function i()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function j()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("搜索数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0, -1)
+gg.searchNumber("改善数值", gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll("修改数值",gg.TYPE_FLOAT)
+gg.toast("开启成功")
+end
+
+
+
+function Exit()
+print("ɪsɪ ғᴏʀᴍᴀᴛ sᴇsᴜᴀɪ ᴋᴇɪɴɢɪɴᴀɴ ᴋᴀᴍᴜ")
+os.exit()
+end
+cs = "这里可以改成你的QQ"
+
+
+
+while true do
+  if gg.isVisible(true) then
+    XGCK = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if XGCK == 1 then
+    Main()
+  end
+end
+
+
+
+
+
+
+
+
+
