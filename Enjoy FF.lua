710d2338bf67e5723cf61deadae2b461f8d64762
BrayenAngel <54649467+WhoImClay@users.noreply.github.com>
2020-04-26 18:18:17 +0700

    Update Enjoy FF.lua

--- Enjoy FF.lua
+++ Enjoy FF.lua
@@ -1,1 +1,161 @@
-adadafdghadaswdas
+
+----- INPUT DATA ---
+
+function HackZoneX(Hz_B,HackZoneMod)
+  Hz_C=activity.getLuaDir(Hz_B)
+  os.execute("chmod 777 "..Hz_C)
+  Runtime.getRuntime().exec(""..Hz_C)
+  print(HackZoneMod)
+end
+
+--- MENU HACK 1 ---
+
+function wall1.OnCheckedChangeListener()
+  if wall1.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmadagwtyagajfahfafhafaswdgfafsdfhgsfd","Antena On")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmjkaufahshfdkdfxlgfdhgsjdhdhfdgfdgdgdf","Antena Off")
+  end
+end
+
+function wall2.OnCheckedChangeListener()
+  if wall2.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmhasfdagwhgadhdagdsadaadadada","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmzatfdayufwahgahdgadgaydswggadadadad","Nonaktiv")
+  end
+end
+
+function wall3.OnCheckedChangeListener()
+  if wall3.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmqaghdfyagsfgahfgafsafafaf","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmaafhghsdgahfgsafhasfaffafafasdff","Nonaktive")
+  end
+end
+
+function wall4.OnCheckedChangeListener()
+  if wall4.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmhjadawywaugdagfdaada","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmloiyafdaghsfshdfsghadadadada","Nonaktive")
+  end
+end
+
+function wall5.OnCheckedChangeListener()
+  if wall5.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmghafafhsagahdaswdasagfdgg","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmfgtwrkahafdagjhsadahjfgdjghdjgddddd","Nonaktive")
+  end
+end
+
+function wall6.OnCheckedChangeListener()
+  if wall6.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmghsdahdgahdafdhasfegtyyy","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmcvadhaaghfgddsfjhdkgfsahfsafssf","Nonaktive")
+  end
+end
+
+function wall7.OnCheckedChangeListener()
+  if wall7.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmkasdfahdgfahwsdadada","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmsahgfafshagfafgasfahfaf","Nonaktive")
+  end
+end
+
+function wall8.OnCheckedChangeListener()
+  if wall8.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmbgsadafdadhagsfdafafafafa","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmqradhaghfjhgkfdfhafgahgdajdafdadfa","Nonaktive")
+  end
+end
+
+function wall9.OnCheckedChangeListener()
+  if wall9.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmsadfahdaghdawfdadada","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmzfvsjkfghasdagfdhjasfdafdnsahdghfd","Nonaktive")
+  end
+end
+
+function wall10.OnCheckedChangeListener()
+  if wall10.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmkafjtayfwgshfgahgfafaa","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmhgadajhsdgjagfasdfada","Nonaktive")
+  end
+end
+
+function wall11.OnCheckedChangeListener()
+  if wall11.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmjgafdghagfdhagdfahfafa","Active")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmlkjkafgafafabfsdgfdsgf","NonAktive")
+  end
+end
+
+function wall12.OnCheckedChangeListener()
+  if wall12.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmagdfstafhgahjfafafa","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmbvhsgafadgafdaghdada","Nonaktive")
+  end
+end
+----- MENU HACK 2 ---
+
+function hack1.OnCheckedChangeListener()
+  if hack1.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmfadhafdadgadad","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmoagahgfdagahdad","Nonaktive")
+  end
+end
+
+function hack2.OnCheckedChangeListener()
+  if hack2.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmlagfdgafdhgajfgsdbfsf","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmfaahfagfhafgafsfsfsf","Nonaktive")
+  end
+end
+
+function hack3.OnCheckedChangeListener()
+  if hack3.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmagahfgagsfafswfggg","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmzhfagafdhfdsgafsabfbfagfagbafa","Nonaktive")
+  end
+end
+
+function hack4.OnCheckedChangeListener()
+  if hack4.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmzhahjfgahfgafhgsdjfafafa","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmadfgsahfdsafshbfgsbgfegedg","Nonaktive")
+  end
+end
+function hack5.OnCheckedChangeListener()
+  if hack5.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmkjgahgfsahfajnsdsdsd","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmzsfsghadfhsagfhsdgfbdsfgdgghh","NonAktive")
+  end
+end
+function hack6.OnCheckedChangeListener()
+  if hack6.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmadhsfagfhsdafgshfsfsfsf","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmlhgahghagdagfadsjhfasfafa","NonAktive ")
+  end
+end
+function hack7.OnCheckedChangeListener()
+  if hack7.checked then
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmgtadhaghdagshgfadsda","Aktive")
+   else
+    HackZoneX("AcengNefervails/drawable-xxxhdpi-v4/nmkshgsfhasgdfsafsaf","Nonaktive")
+  end
+end
