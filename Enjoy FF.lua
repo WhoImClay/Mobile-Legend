68ab93f77ea6947af0e598411f9a0cff8fcf1f70
BrayenAngel <54649467+WhoImClay@users.noreply.github.com>
2020-04-27 19:43:41 +0700

    Update Enjoy FF.lua

--- Enjoy FF.lua
+++ Enjoy FF.lua
@@ -1,1 +1,54 @@
-afadf
+--string ofs_Aimbot
+public virtual float c}hjvA}() { } // RVA: 0x1341AD4 Offset: 0x1341AD4
+
+--string rain bullets
+public int prb|j[() { } // RVA: 0x134210C Offset: 0x134210C
+
+--string fast fire
+public float A}pYJZ() { } // RVA: 0x13420D8 Offset: 0x13420D8
+
+--string ghost
+private bool yDYdhFu(Player sAbRLZE) { } // RVA: 0x17097F4 Offset: 0x17097F4
+
+--Ampliar Vista
+protected virtual float o~ym{jW() { } // RVA: 0x133CA58 Offset: 0x133CA58
+
+--Remover Neblina /Kabut
+public static bool get_fog() { } // RVA: 0x26925BC Offset: 0x26925BC
+
+--hapus hierba / rumput
+private void thVkTK](Vector2 w]MHUK~, Vector2 vYwsFZ^, ref Vector2 xiGTXf, ref Vector2 jp]IXDa, float VvokKIu) { } // RVA: 0x10E1788 Offset: 0x10E1788
+
+--medkit run 2 ofs
+private void OnPreparationCancel(object[] param) { } // RVA: 0x21445A4 Offset: 0x21445A4 //ATAS NO 1
+public bool IsMoving() { } // RVA: 0xAD1B78 Offset: 0xAD1B78// BAWAH no 2
+
+--White Body ALL player
+public bool UseHighQualityAvatar() { } // RVA: 0xAB5908 Offset: 0xAB5908
+
+--Fix Lag Shot
+public void KZORFBE(FMGGm[D Bq^sk~z, MonoBehaviour |^gxoKu, float BvfQcJK) { } // RVA: 0x134B3C0 Offset: 0x134B3C0
+
+--ammo fast 
+public float hOU}lE{() { } // RVA: 0x134211C Offset: 0x134211C
+
+--shoot move
+public bool IsFreeMove() { } // RVA: 0xAD8900 Offset: 0xAD8900
+
+--quick change
+public void OnStartChangeWeapon(fsn{~u uosduy) { } // RVA: 0xAE4EF4 Offset: 0xAE4EF4
+
+--fast punch  
+public float eWsKez() { } // RVA: 0x1341E74 Offset: 0x1341E74
+
+--diamond
+public int get_UserGems() { } // RVA: 0xF8DAA0 Offset: 0xF8DAA0
+
+--gold
+public uint get_UserCoins() { } // RVA: 0xFB8F78 Offset: 0xFB8F78
+
+--badge
+public uint get_badge_cnt() { } // RVA: 0x286CB34 Offset: 0x286CB34  // NO 1 DARI HASIL
+
+--score 
+public uint get_ranking_points() { } // RVA: 0x286CF6C Offset: 0x286CF6C // NO 2 DARI HASIL
